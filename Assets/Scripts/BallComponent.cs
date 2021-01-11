19e63ea9551122d9e7bf92c93635617257531889
reychel <rychu03@gmail.com>
2021-01-11 14:44:45 +0100

    FPS and change scale scripts.

--- Assets/Scripts/BallComponent.cs
+++ Assets/Scripts/BallComponent.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class BallComponent : MonoBehaviour
 {
+
+    public float Speed = 1.0f;
+    public float RotationSpeed = 10.0f;
+    public float SizeChangeSpeed = 1.0f;
+    private Vector3 _vecRotation = Vector3.zero;
+    private Vector3 _desirableScaleVector = new Vector3();
+
+    public void ChangeScaleOfObject(Vector3 desirableScaleVector, Transform gameObjectTransform, float Speed)
+    {
+        Vector3 deltaScaleVector = desirableScaleVector - gameObjectTransform.localScale;
+
+        if(deltaScaleVector.x >= 0.01)
+        {
+            gameObjectTransform.localScale += deltaScaleVector * Speed * Time.deltaTime;
+        }
+        else
+        {
+            gameObjectTransform.localScale = desirableScaleVector;
+        }
+        
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello World!");
+        //Debug.Log("Hello World!");
+        _desirableScaleVector.Set(3.0f, 0.1f, 3.0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Czas od ostatniej klatki: " + Time.deltaTime);
+        //Debug.Log("Czas od ostatniej klatki: " + Time.deltaTime);
+
+        _vecRotation += Vector3.forward * RotationSpeed;
+        transform.rotation = Quaternion.Euler(_vecRotation);
+
+        transform.position += Vector3.up * Time.deltaTime * Speed;
+
+        ChangeScaleOfObject(_desirableScaleVector, transform, SizeChangeSpeed);
+
+        
+
+
     }
 }
---
+++ Assets/Scripts/FramesPerSeconds.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FramesPerSeconds : MonoBehaviour
+{
+
+    float timerTank;
+    int frame;
+    public float FPS;
+    float anotherFPS;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timerTank = 0.0f;
+        frame = 0;
+        anotherFPS = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ++frame;
+        //Debug.Log("Frames passed: " + frame);
+        
+        timerTank += Time.deltaTime;
+
+        FPS = 1 / Time.deltaTime;
+        //Debug.Log("FPS: " + FPS);
+
+        if (timerTank > 1.0f)
+        {
+            anotherFPS = frame / timerTank;
+
+            Debug.Log("Frames per seconds: " + frame + " /anotherFPS: " + anotherFPS);
+            frame = 0;
+            timerTank = 0;
+        }
+
+    }
+}
